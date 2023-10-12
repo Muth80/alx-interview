46d3128b14b935912174128557875b86546fb126
David Muindi <muindidiego@gmail.com>
2023-10-12 03:07:01 -0700

    Tasks

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Calculates the fewest number of operations needed to result in exactly n H
+characters in the file.
+"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    dp = [0] * (n + 1)
+    for i in range(2, n + 1):
+        dp[i] = i
+        for j in range(2, i // 2 + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+
+    return dp[n]
