f4eb13d7a13d26e9a3c485b892b84408b88674fa
David Muindi <muindidiego@gmail.com>
2023-10-26 03:07:54 -0700

    Tasks

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,10 +2,11 @@
 
 def validUTF8(data):
     count = 0
-    for i in data:
-        i = format(i, '#010b')[-8:]
+    for byte in data:
+        byte = byte & 0b11111111
+        byte = format(byte, '#010b')[-8:]
         if count == 0:
-            for bit in i:
+            for bit in byte:
                 if bit == '0':
                     break
                 count += 1
@@ -14,7 +15,7 @@ def validUTF8(data):
             if count == 1 or count > 4:
                 return False
         else:
-            if not (i[0] == '1' and i[1] == '0'):
+            if not (byte[0] == '1' and byte[1] == '0'):
                 return False
         count = count - 1 if count > 0 else 0
     if count == 0:
