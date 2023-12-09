bf72f7f0a010e0901f52b285d5ef9696888bce73
David Muindi <muindidiego@gmail.com>
2023-12-09 10:54:29 +0000

    Tasks

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,43 +1,58 @@
 #!/usr/bin/python3
-
 def isWinner(x, nums):
-    if not nums:
-        return None
-
-    # A list to keep track of prime numbers in nums
-    primes = []
-
-    # Check each number in nums to see if it is prime
-    for num in nums:
-        if num > 1:
-            for i in range(2, num):
-                if (num % i) == 0:
+    # Calculate the next prime number after n
+    def nextPrime(n):
+        if n <= 1:
+            return 2
+        if n % 2 == 0:
+            n += 1
+        while True:
+            isPrime = True
+            sqrt_n = int(n**0.5) + 1
+            for i in range(3, sqrt_n, 2):
+                if n % i == 0:
+                    isPrime = False
                     break
-            else:
-                primes.append(num)
+            if isPrime:
+                return n
+            n += 2
 
-    # A dictionary to keep track of each player's scores
-    scores = {"Maria": 0, "Ben": 0}
+    # Function to determine the winner of a single round
+    def roundWinner(n):
+        primeCount = 0
+        maxPrime = 0
+        for num in nums:
+            if num <= n:
+                primeCount += 1
+                maxPrime = max(maxPrime, num)
+        return 'Maria' if primeCount == 1 else 'Ben' if primeCount > 1 else 'None'
 
-    # Simulate the game rounds
-    for i in range(x):
-        current_round = i % 2
-        if not primes:
-            scores[["Maria", "Ben"][current_round]] += 1
-            continue
+    # Initialize the result dictionary
+    results = {'Maria': 0, 'Ben': 0, 'None': 0}
 
-        prime = primes.pop(0)
-        removed = [prime * i for i in range(1, prime + 1)]
-        for num in nums[:]:
-            if num in removed:
-                nums.remove(num)
+    # Iterate through each round and calculate the winner
+    for num in nums:
+        if num <= 1:
+            results['Ben'] += 1
+        else:
+            n = nextPrime(num)
+            winner = roundWinner(n)
+            results[winner] += 1
 
-        scores[["Maria", "Ben"][(current_round + 1) % 2]] += 1
-
-    # Determine the winner
-    if scores["Maria"] > scores["Ben"]:
-        return "Maria"
-    elif scores["Maria"] < scores["Ben"]:
-        return "Ben"
+    # Determine the player with the most wins
+    if results['Maria'] > results['Ben']:
+        return 'Maria'
+    elif results['Maria'] < results['Ben']:
+        return 'Ben'
     else:
-        return None
+        return 'None'
+
+if __name__ == "__main__":
+    import sys
+    import cProfile
+    if len(sys.argv) == 3:
+        x = int(sys.argv[1])
+        nums = list(map(int, sys.argv[2].split(',')))
+        cProfile.run('print("Winner: {}".format(isWinner(x, nums)))')
+    else:
+        print("Usage: {} <number_of_rounds> <comma_separated_numbers>".format(sys.argv[0]))
