068da36eed2a8305e6ff0946a2a8dffe08731f2f
David Muindi <muindidiego@gmail.com>
2024-03-08 23:47:37 +0300

    Task

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,44 +1,15 @@
 #!/usr/bin/python3
+import random
 import sys
+from time import sleep
+import datetime
 
-status_codes_count = {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
-total_size = 0
-lines_processed = 0
-
-try:
-    for line in sys.stdin:
-        parts = line.split()
-        if len(parts) < 7 or '"' not in line:
-            continue
-        status_code = parts[-2]
-        file_size = parts[-1]
-
-        # Try to parse the file size and update metrics if successful
-        try:
-            total_size += int(file_size)
-            if status_code in status_codes_count:
-                status_codes_count[status_code] += 1
-        except ValueError:
-            # Skipping the line if file_size is not a number, as per requirements
-            continue
-        
-        lines_processed += 1
-        if lines_processed % 10 == 0:
-            print("File size: {}".format(total_size))
-            for code in sorted(status_codes_count.keys()):
-                if status_codes_count[code] > 0:
-                    print("{}: {}".format(code, status_codes_count[code]))
-
-except KeyboardInterrupt:
-    # This will handle the Ctrl + C interruption and print the metrics
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes_count.keys()):
-        if status_codes_count[code] > 0:
-            print("{}: {}".format(code, status_codes_count[code]))
-    raise
-
-# Finally, print the metrics before the program exits naturally
-print("File size: {}".format(total_size))
-for code in sorted(status_codes_count.keys()):
-    if status_codes_count[code] > 0:
-        print("{}: {}".format(code, status_codes_count[code]))
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
